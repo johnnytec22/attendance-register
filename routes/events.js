c44e8d3456aa4a4fba6763aa8f2dfdaeae4a98c3
John Obi <johnny9334@ymail.com>
2018-04-24 18:25:39 +0100

    event page view, event editing functionality

--- routes/events.js
+++ routes/events.js
@@ -5,10 +5,10 @@ var admin = require('firebase-admin')
 
 
 router.get('/event/:key', (req, res) => {
-    var id = req.params.key;
+    var key = req.params.key;
 
     var db = admin.database();
-    var ref = db.ref('registerapp').child('events').child(id);
+    var ref = db.ref('registerapp').child('events').child(key);
 
     ref.once('value', (event_data) => {
         var data = event_data.val();
@@ -17,6 +17,36 @@ router.get('/event/:key', (req, res) => {
         res.render('event_info.pug', {'data':data})
     })
 }) 
+
+router.get('/:key/edit', (req, res) => {
+    var key = req.params.key;
+
+    var db = admin.database();
+    var ref = db.ref('registerapp').child('events').child(key);
+
+    ref.once('value').then((snap) => {
+        var data = snap.val()
+        data['key'] = snap.key;
+
+        res.render('event_edit.pug', {'data':data});
+    })
+
+});
+
+router.post('/:key/update', (req, res) => {
+    var key = req.params.key;
+
+    var db = admin.database();
+    var ref = db.ref('registerapp').child('events').child(key);
+
+    ref.update(req.body).then(function(doc) {
+        res.redirect('/events/'+key+'/edit')
+    }).catch(function(error) {
+        res.send('error: Please, try again')
+    })
+ 
+});
+
 
 
 module.exports = router;
